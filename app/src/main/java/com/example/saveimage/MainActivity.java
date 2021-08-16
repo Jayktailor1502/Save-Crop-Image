4edc4a2eb3d00c438ae1d05817cc14bb6203d0da
jayktailor1502 <jay.kumar@unthinkable.co>
2021-08-16 17:58:46 +0530

    2commit

--- app/src/main/java/com/example/saveimage/MainActivity.java
+++ app/src/main/java/com/example/saveimage/MainActivity.java
@@ -1,14 +1,128 @@
 package com.example.saveimage;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.util.Objects;
+
+import static android.os.Environment.DIRECTORY_PICTURES;
+import static androidx.core.content.FileProvider.getUriForFile;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView imageView;
+    Button btnSave;
+    public static long fileName;
+    OutputStream fos;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageView = findViewById(R.id.photoView);
+        btnSave = findViewById(R.id.btnSave);
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BitmapDrawable bitmapDrawable = (BitmapDrawable) imageView.getDrawable();
+                Bitmap bitmap = bitmapDrawable.getBitmap();
+                saveToGallery(bitmap);
+            }
+        });
     }
+
+    private void saveToGallery(Bitmap bitmap) {
+        try{
+            fileName = System.currentTimeMillis();
+            ContentResolver contentResolver = getContentResolver();
+            ContentValues contentValues = new ContentValues();
+            contentValues.put(MediaStore.MediaColumns.DISPLAY_NAME, "IMG"+fileName + ".jpg");
+            contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg");
+            contentValues.put(MediaStore.MediaColumns.RELATIVE_PATH, DIRECTORY_PICTURES + File.separator + "DemoFolder");
+            Uri imageURI = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues);
+
+            fos = contentResolver.openOutputStream(Objects.requireNonNull(imageURI));
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, fos);
+            Objects.requireNonNull(fos);
+            fos.flush();
+            fos.close();
+            Toast.makeText(MainActivity.this, "Image Saved", Toast.LENGTH_LONG).show();
+        }
+        catch (Exception e)
+        {
+            Toast.makeText(MainActivity.this, "Image Not Saved", Toast.LENGTH_LONG).show();
+            Log.e("Hello","Exception - " + e);
+        }
+
+    }
+
+    ActivityResultLauncher<Intent> launchCamera = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if(result.getResultCode() == Activity.RESULT_OK) {
+                        Intent data = result.getData();
+                        assert data != null;
+                        Bitmap photo1 = (Bitmap) data.getExtras().get("data");
+                        imageView.setImageBitmap(photo1);
+                    }
+                }
+            });
+
+    public void takePictureFromCamera(View view) {
+        Dexter.withContext(this)
+                .withPermission(Manifest.permission.CAMERA)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                        launchCamera.launch(takePictureIntent);
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+                        if(permissionDeniedResponse.isPermanentlyDenied()) {
+                            Toast.makeText(MainActivity.this,"Please allow permissions",Toast.LENGTH_LONG).show();
+                        }
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+                        permissionToken.continuePermissionRequest();
+                    }
+                }).check();
+    }
+
+
 }
